4c9414cbcc2fe93ea7486a398cf7219887d0cdce
Augusto <augustoramos067@gmail.com>
2025-07-09 15:49:28 -0300

    correcao do http request

--- src/CriadorDeRequisicao.java
+++ src/CriadorDeRequisicao.java
@@ -3,7 +3,7 @@ import java.net.http.HttpRequest;
 
 public class CriadorDeRequisicao {
     public static HttpRequest criar(String moedaBase, String moedaAlvo) {
-        String url = "https://v6.exchangerate-api.com/v6/97544bc55eca2c963b0320a8/pair/" + moedaBase + "/" + moedaAlvo;
+        String url = "https://v6.exchangerate-api.com/v6/SUA_CHAVE_AQUI/pair/" + moedaBase + "/" + moedaAlvo;
         return HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
